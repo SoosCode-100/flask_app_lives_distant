93e00e244304b66988a9764eef7d7866b133bce5
Soos <Salmonellaoverdose@gmail.com>
2026-03-31 10:39:40 +0100

    i have a good feeling

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ try { docker stop flask_app_lives } catch {}
 try { docker rm flask_app_lives } catch {}
 
 # Tirer la dernière image
-docker pull sooscode/flask-app_lives:latest
+docker pull sooscode/flask_app_lives:latest
 
 # Lancer le conteneur
 docker run -d --name flask_app_lives -p 5000:5000 sooscode/flask-app_lives:latest
