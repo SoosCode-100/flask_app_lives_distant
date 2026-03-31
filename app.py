4d55ce5dc97282118399aefb9bf618bdef3b86b5
Soos <Salmonellaoverdose@gmail.com>
2026-03-31 10:47:46 +0100

    new comment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello, devops - Docker CI Pipeline! Second test'
+    return 'hello, devops - Docker CI Pipeline! third test'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
