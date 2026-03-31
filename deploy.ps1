4da145d87cf15f12de2910497e967dddbd47b159
Soos <Salmonellaoverdose@gmail.com>
2026-03-31 10:41:53 +0100

    i have a good feeling1

--- deploy.ps1
+++ deploy.ps1
@@ -6,4 +6,4 @@ try { docker rm flask_app_lives } catch {}
 docker pull sooscode/flask_app_lives:latest
 
 # Lancer le conteneur
-docker run -d --name flask_app_lives -p 5000:5000 sooscode/flask-app_lives:latest
+docker run -d --name flask_app_lives -p 5000:5000 sooscode/flask_app_lives:latest
