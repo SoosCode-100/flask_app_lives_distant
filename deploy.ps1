df1c865be19e1d596666f56bfad5e3152e3a7134
Soos <Salmonellaoverdose@gmail.com>
2026-03-31 09:47:48 +0100

    Changed Docker yml and deploy ps to reflect file and user names

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
 # Arrêter et supprimer l'ancien conteneur
-try { docker stop flask_app } catch {}
-try { docker rm flask_app } catch {}
+try { docker stop flask_app_lives } catch {}
+try { docker rm flask_app_lives } catch {}
 
 # Tirer la dernière image
-docker pull votre_UsernameDocker/flask_devops_cd:latest
+docker pull sooscode/flask-app_lives:latest
 
 # Lancer le conteneur
-docker run -d --name flask_app -p 5000:5000 votre_UsernameDocker/flask_devops_cd:latest
+docker run -d --name flask_app_lives -p 5000:5000 sooscode/flask-app_lives:latest
